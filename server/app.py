6410fc947a58211f1c5da25f304071d0925d00e5
James Kamau <jamesnk5773@gmail.com>
2025-09-15 20:30:36 +0300

    add view and routes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,26 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def earthquake_view(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return make_response(jsonify(earthquake.to_dict()), 200)
+    else:
+        return make_response(jsonify({'message': f'Earthquake {id} not found.'}), 404)
+    
 
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])   
+def get_earthquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
 
+    earthquakes_data = [quake.to_dict() for quake in earthquakes]
+
+    response_data = {
+        'count': len(earthquakes_data),
+        'quakes': earthquakes_data
+    }
+    return make_response(jsonify(response_data), 200)
+    
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
